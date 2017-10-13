ff4efe402d31fcc0bcd983087a4eaa5a99235b8c
mahimasunnym <mahimasunnym@gmail.com>
2017-10-12 23:50:13 -0700

    iiiiii

--- code.js
+++ code.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
   database : 'MedicalData'
 });
 connection.connect();
-
+//aaaa
 var queryString = 'SELECT * FROM Readings';
 //getdata
 function getData(callback) {
